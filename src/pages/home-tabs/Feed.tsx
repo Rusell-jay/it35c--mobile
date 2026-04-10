4ec2fd02759b06023b0e84be834d2ba69c3a22e4
Rusell-jay <chingruselljay@gmail.com>
2026-04-10 23:36:29 +0800

    added some items in feed

--- src/pages/home-tabs/Feed.tsx
+++ src/pages/home-tabs/Feed.tsx
@@ -1,20 +1,39 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle } from "@ionic/react";
+import { IonButtons, IonContent, IonHeader, IonItem, IonLabel, IonList, IonMenuButton, IonPage, IonTitle, IonToolbar } from "@ionic/react";
 
 
 const Feed: React.FC = () => {
+
+  const games = [
+    { name: 'Pokemon Yellow' },
+    { name: 'Mega Man X' },
+    { name: 'The Legend of Zelda' },
+    { name: 'Pac-Man' },
+    { name: 'Super Mario World' },
+    { name: 'Bomberman' }
+  ];
+
   return (
     <IonPage>
-        <IonHeader>
-            <IonButtons>
-                <IonMenuButton></IonMenuButton>
-                <IonTitle>Feed</IonTitle>
-            </IonButtons>
-        </IonHeader>
-        <IonContent className="ion-padding">
-            <h1>Feed</h1>
-        </IonContent>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons>
+            <IonMenuButton></IonMenuButton>
+            <IonTitle>Feed</IonTitle>
+          </IonButtons>
+        </IonToolbar>
+      </IonHeader>
+
+      <IonContent className="ion-padding">
+        <IonList>
+          {games.map((item, index) => (
+            <IonItem key={index}>
+              <IonLabel>{item.name}</IonLabel>
+            </IonItem>
+          ))}
+        </IonList>
+      </IonContent>
     </IonPage>
-  )
+  );
 };
 
 export default Feed;
